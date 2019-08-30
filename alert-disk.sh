de7d61dff4631e57dcf9bc22c9ca140b0a4a6714
Paolo Basso <basso.paolo99@gmail.com>
2019-08-30 14:36:17 +0200

    Fix alert-disk

--- alert-disk.sh
+++ alert-disk.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-MAIL=mailid@domainname.com
 DIR=/home
 CURRENT=$(df $DIR | grep $DIR | awk '{ print $5}' | sed 's/%//g')
 PERCENTAGE=85
 SERVER_NAME='Awesome server'
 CHECK_URL=https://hc-ping.com/123456789
+KEY=123456789
+URL=https://example.com
 
 if [ "$CURRENT" -gt "$PERCENTAGE" ] ; then
-    echo Your disk remaining free space is low. Used: $CURRENT% | mail -s 'Disk Space Alert for $SERVER_NAME' $MAIL
+    curl -X POST -H "Content-Type: application/json" -d "{\"key\":\"$KEY\", \"title\":\"Disk alert $SERVER_NAME\", \"msg\":\"Your disk remaining free space is low. Used: $CURRENT%\"}" $URL
 fi
 
 # Check url
